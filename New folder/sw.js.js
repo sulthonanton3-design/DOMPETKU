a5dd7204cf48ce90aa3feda3fdda963bd0aea895
sulthonanton3-design <sulthonanton3@gmail.com>
2026-08-21 16:07:14 +0700

    Add files via upload

---
+++ New folder/sw.js.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'dompetku-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
